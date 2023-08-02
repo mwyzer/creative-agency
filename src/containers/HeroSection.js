6d0acf6762601ad258a32bcacedf30bc307a51a9
Muhammad Wyzer <muhammad.wyzer@gmail.com>
2023-08-02 08:20:10 +0700

    test

---
+++ src/containers/HeroSection.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import shapeCircleBlue from "@/assets/images/shape-circle_blue.png";
+import shapeCircleRed from "@/assets/images/shape-circle_red.png";
+import heroSectionImage from "@/assets/images/heroSectionImage.png";
+
+const HeroSection = () => {
+  return (
+    <section id="hero">
+      <div className="container">
+        <div>
+          <p className="title">Make your dream business goal come true</p>
+          <p className="description">
+            when you need us for improve your business, then come with us to
+            help your business have reach it, you just sit and feel that goal.
+          </p>
+          <Link href="/" className="button-contained">
+            Start Project
+          </Link>
+          <div>
+            <Image className="shape-blue" src={shapeCircleBlue} alt="" />
+            <Image className="shape-red" src={shapeCircleRed} alt="" />
+          </div>
+        </div>
+        <Image className="image" src={heroSectionImage} alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
